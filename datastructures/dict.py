edff2596ca9367772f41bf9c78bf588cacfd7f41
hamzza21 <35273943+hamzza21@users.noreply.github.com>
2025-11-26 23:18:32 +0500

    updated the dictionary file

--- datastructures/dict.py
+++ datastructures/dict.py
@@ -1,8 +1,17 @@
 """Dictionary Data Structure in Pyhton"""
-
+# creating a dictionary
 dict1 = {
     1:"A",
     2:"B",
     3:"C"
     }
+
+print(dict1)
+
+# adding an item to the dictionary
+dict1[4] = "D"
+print(dict1)
+
+# upating an item in the dictionary
+dict1[4] = "E"
 print(dict1)
